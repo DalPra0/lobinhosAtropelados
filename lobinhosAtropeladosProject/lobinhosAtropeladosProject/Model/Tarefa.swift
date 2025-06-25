a69441018857dfc4e597c770cb7f113b2b42dc67
beatrizperottomuniz <beatrizperottomuniz@gmail.com>
2025-06-25 16:54:04 -0300

    Mudanca Model
    Mudanca do Model de tarefa (let-> var) e lita de tarefas.

--- lobinhosAtropeladosProject/lobinhosAtropeladosProject/Model/Tarefa.swift
+++ lobinhosAtropeladosProject/lobinhosAtropeladosProject/Model/Tarefa.swift
@@ -9,10 +9,10 @@ import Foundation
 
 struct Tarefa: Identifiable {
     let id = UUID()
-    let nome: String
-    let descricao: String?  // Opcional
-    let duracao_minutos: Int   // para usar em hora divida por 60
-    let dificuldade: String     //texto botao
+    var nome: String
+    var descricao: String?  // Opcional
+    var duracao_minutos: Int   // para usar em hora divida por 60
+    var dificuldade: String     //texto botao
     var esforco: String        // texto botao
     var prioridade : Int?      // o que a IA vai alterar -> null (nil no swift) ou int
 }
