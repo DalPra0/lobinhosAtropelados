e6b4189654b2a161cc08d64eba3a74c5f2f4e0b2
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-06-25 15:30:18 -0300

    Initial commit

---
+++ lobinhosAtropeladosProject/lobinhosAtropeladosProject/lobinhosAtropeladosProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  lobinhosAtropeladosProjectApp.swift
+//  lobinhosAtropeladosProject
+//
+//  Created by Lucas Dal Pra Brascher on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct lobinhosAtropeladosProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
