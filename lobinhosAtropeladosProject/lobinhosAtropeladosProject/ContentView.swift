ae03bf6d94e1b5f71651c7cc5c035642aa675bf8
beatrizperottomuniz <beatrizperottomuniz@gmail.com>
2025-06-26 14:56:20 -0300

    TabBar + Perfil + Modal (placeholder)
    Foi colocada a TabBar na content View, com opcoes de home (lista principal) , adicao de tarefas e perfil (placeholder) . O modal é um placeholder, será modificado para ser aberto quando formos adicionar/editar uma tarefa

--- lobinhosAtropeladosProject/lobinhosAtropeladosProject/ContentView.swift
+++ lobinhosAtropeladosProject/lobinhosAtropeladosProject/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showModal = false
+    @State private var selectedTab: Int = 0 // Início na primeira aba
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        TabView(selection: $selectedTab) {
+            //TelaIncialView()
+            Text("Tela inicial View")
+                .tabItem {
+                    Image(systemName: "house")
+                }.tag(0)
+            Text("Hello, World!")
+                .tabItem {
+                    Image(systemName: "plus.circle")
+                }.tag(1)
+            PerfilView()
+                .tabItem {
+                    Image(systemName: "person.circle")
+                }.tag(2)
+        }.accentColor(.blue)
+            .sheet(isPresented: $showModal) {
+                // Conteúdo da sheet
+                TarefaModalView()
+                    .presentationDetents([.large]) //sheet ocupe a tela inteira
+            }
+            .onChange(of: selectedTab) {
+                if selectedTab == 1 {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+                        self.showModal = true
+                        self.selectedTab = 0
+                    }
+                }
+            }
     }
 }
 
