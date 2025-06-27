4de6b3b9abba3ade42e0b1e425d0bbec3631b5a7
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-06-27 15:21:24 -0300

    Initial project setup with SwiftUI template
    Set up a new Xcode project for 'lobinhosAtropelados' using the SwiftUI app template. Added default ContentView, app entry point, and asset catalog files.

---
+++ lobinhosAtropelados/lobinhosAtropeladosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lobinhosAtropeladosApp.swift
+//  lobinhosAtropelados
+//
+//  Created by Lucas Dal Pra Brascher on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct lobinhosAtropeladosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
